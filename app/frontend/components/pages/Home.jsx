cac3701b8854347007cd7dda0d9af68233bd1efc
Anthony You <you.anthony@yahoo.com>
2022-11-16 18:37:38 +0100

    CYC-29 wip

--- app/frontend/components/pages/Home.jsx
+++ app/frontend/components/pages/Home.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const Home = () => {
-  return <h1>Hello from home page</h1>
+  return (
+    <div className="flex">
+      <h1 className="mr-5">Hello from home page</h1>
+      <Button variant="contained">Hello world!</Button>
+    </div>
+  )
 };
 
 export default Home;
