807018db7d547d43a7f18891bcbe5310d428b90e
Anthony You <you.anthony@yahoo.com>
2022-11-22 21:05:40 +0100

    wip

--- app/frontend/components/pages/Home.tsx
+++ app/frontend/components/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../UI/Button";
+import Cyclist from "../../images/biking.svg";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <Button color="primary" className="mr-2">Je souhaite louer</Button>
         <Button color="secondary">Je suis propriétaire</Button>
       </div>
+      <img src={Cyclist} alt="Cyclist" />
     </div>
   )
 };
