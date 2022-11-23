598552798901e12ee60129189ab8b4345a34558d
Anthony You <you.anthony@yahoo.com>
2022-11-23 09:51:55 +0100

    wip

--- app/frontend/components/pages/Home.tsx
+++ app/frontend/components/pages/Home.tsx
@@ -4,14 +4,16 @@ import Cyclist from "../../images/biking.svg";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Et si on louait un vélo ?</h1>
-      <p>Louer un vélo entre particuliers n'a jamais été aussi simple.</p>
-      <div className="flex">
-        <Button color="primary" className="mr-2">Je souhaite louer</Button>
-        <Button color="secondary">Je suis propriétaire</Button>
+    <div className="grid grid-cols-1 sm:grid-cols-2 items-center">
+      <div>
+        <h1>Et si on louait un vélo ?</h1>
+        <p>Louer un vélo entre particuliers n'a jamais été aussi simple.</p>
+        <div className="flex">
+          <Button color="primary" className="mr-2">Je souhaite louer</Button>
+          <Button color="secondary">Je suis propriétaire</Button>
+        </div>
       </div>
-      <img src={Cyclist} alt="Cyclist" />
+      <img src={Cyclist} alt="Cyclist" className="w-full" />
     </div>
   )
 };
