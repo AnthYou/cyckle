1fe87e5c7f0758a291b30579dd8aa76954c15922
Anthony You <you.anthony@yahoo.com>
2022-11-23 09:54:12 +0100

    wip

--- app/frontend/components/layout/Footer.tsx
+++ app/frontend/components/layout/Footer.tsx
@@ -4,25 +4,25 @@ import classes from "./Footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer
-      className={`${classes.Footer} flex flex-col text-center p-2 sm:flex-row sm:text-left sm:p-10`}
-    >
-      <div className={classes.logo}>
-        <h3>Cyckle</h3>
-        <p>Location de vélos entre particuliers</p>
-      </div>
-      <div className="flex flex-1 justify-between">
-        <ul className="sm:mr-4">
-          <li>S'inscrire / se connecter</li>
-          <li>Blog</li>
-          <li>Nous rejoindre</li>
-        </ul>
-        <ul>
-          <li>Contactez-nous</li>
-          <li>F.A.Q.</li>
-          <li>Politique de confidentialité</li>
-          <li>Mentions légales</li>
-        </ul>
+    <footer className={`${classes.Footer}`}>
+      <div className="container flex flex-col text-center sm:flex-row sm:text-left">
+        <div className={classes.logo}>
+          <h3>Cyckle</h3>
+          <p>Location de vélos entre particuliers</p>
+        </div>
+        <div className="flex flex-1 justify-between">
+          <ul className="sm:mr-4">
+            <li>S'inscrire / se connecter</li>
+            <li>Blog</li>
+            <li>Nous rejoindre</li>
+          </ul>
+          <ul>
+            <li>Contactez-nous</li>
+            <li>F.A.Q.</li>
+            <li>Politique de confidentialité</li>
+            <li>Mentions légales</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
