bf64aa9631766c52339888f62e8a895933329149
Anthony You <you.anthony@yahoo.com>
2022-11-24 13:33:36 +0100

    wip

--- app/frontend/components/layout/KeySellingPoint.tsx
+++ app/frontend/components/layout/KeySellingPoint.tsx
@@ -5,7 +5,7 @@ interface KeySellingPointProps {
   title: string;
   content: string;
   image: string;
-  alt: string
+  alt: string;
 }
 
 const KeySellingPoint = ({
@@ -15,13 +15,17 @@ const KeySellingPoint = ({
   image,
   alt,
 }: KeySellingPointProps): JSX.Element => {
+  const wrapperClasses = `flex ${
+    id % 2 === 0 ? "flex-row" : "flex-row-reverse"
+  } items-center justify-between gap-4`;
+
   return (
-    <div>
-      <div>
+    <div className={wrapperClasses}>
+      <div className="w-1/2">
         <h2>{title}</h2>
         <p>{content}</p>
       </div>
-      <img src={image} alt={alt} />
+      <img src={image} alt={alt} className="w-2/5" />
     </div>
   );
 };
