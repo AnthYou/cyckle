93e15a79d87c6c4e1f8d20e87396654b000e6ed2
Anthony You <you.anthony@yahoo.com>
2022-11-29 11:17:37 +0100

    wip

--- app/models/message.rb
+++ app/models/message.rb
@@ -23,5 +23,5 @@ class Message < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', required: true
   belongs_to :booking
 
-  # validates :content, presence: true
+  validates :content, presence: true
 end
